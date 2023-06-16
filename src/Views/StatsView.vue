a7219ffa0cfe52bfd968aa1d34df98d09383182e
leelanarasimha <leela.narsimha@gmail.com>
2023-06-17 00:31:34 +0530

    stats page design

--- src/Views/StatsView.vue
+++ src/Views/StatsView.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>Stats Page</div>
+  <div class="mt-3">
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>Stat</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Total Notes Count</td>
+          <td>{{ notesCount }}</td>
+        </tr>
+        <tr>
+          <td>Total Characters Count</td>
+          <td>{{ totalCharactersCount }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<script setup>
+  import { useNotesStore } from '../stores/NotesStore';
+
+  const notesStore = useNotesStore();
+
+  const notesCount = notesStore.totalNotesCount;
+  const totalCharactersCount = notesStore.totalCharactersCount;
+</script>
--- src/stores/NotesStore.js
+++ src/stores/NotesStore.js
@@ -13,6 +13,18 @@ export const useNotesStore = defineStore('notesStore', () => {
     };
   });
 
+  const totalNotesCount = computed(() => {
+    return notes.value.length;
+  });
+
+  const totalCharactersCount = computed(() => {
+    let count = 0;
+    for (let note of notes.value) {
+      count += note.content.length;
+    }
+    return count;
+  });
+
   const addNote = (noteContent) => {
     const currentDate = new Date().getTime().toString();
     const note = {
@@ -37,6 +49,8 @@ export const useNotesStore = defineStore('notesStore', () => {
     updateNote,
     getNoteContentById,
     addNote,
-    deleteNote
+    deleteNote,
+    totalCharactersCount,
+    totalNotesCount
   };
 });
