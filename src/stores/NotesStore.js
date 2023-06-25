7bcd3f7bf56dca0773730af85206daafbc62934e
leelanarasimha <leela.narsimha@gmail.com>
2023-06-25 09:48:08 +0530

    get realtime notes data

--- src/stores/NotesStore.js
+++ src/stores/NotesStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, onSnapshot } from 'firebase/firestore';
 import { db } from '../js/firebase';
 
 export const useNotesStore = defineStore('notesStore', () => {
@@ -13,13 +13,26 @@ export const useNotesStore = defineStore('notesStore', () => {
   });
 
   const getNotes = async () => {
-    const querySnapshot = await getDocs(collection(db, 'notes'));
-    querySnapshot.forEach((doc) => {
-      let note = {
-        id: doc.id,
-        content: doc.data().content
-      };
-      notes.value.push(note);
+    // const querySnapshot = await getDocs(collection(db, 'notes'));
+    // querySnapshot.forEach((doc) => {
+    //   let note = {
+    //     id: doc.id,
+    //     content: doc.data().content
+    //   };
+    //   notes.value.push(note);
+    // });
+
+    onSnapshot(collection(db, 'notes'), (querySnapshot) => {
+      let notesData = [];
+      querySnapshot.forEach((doc) => {
+        let note = {
+          id: doc.id,
+          content: doc.data().content
+        };
+        notesData.push(note);
+      });
+
+      notes.value = notesData;
     });
   };
 
