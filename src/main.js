452055385b4890596990df9503543c428f427edb
leelanarasimha <leela.narsimha@gmail.com>
2023-07-03 23:51:29 +0530

    global beforeeach navigation guard

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import { createPinia } from 'pinia';
+import { useAuthStore } from './stores/AuthStore';
 
 const app = createApp(App);
+
+router.beforeEach((to, from) => {
+  const authStore = useAuthStore();
+
+  const isAuthenticated = authStore.user.uid ? true : false;
+
+  if (isAuthenticated && to.name === 'auth') {
+    return { name: 'notes' };
+  }
+
+  if (!isAuthenticated && to.name !== 'auth') {
+    return { name: 'auth' };
+  }
+});
 
 const pinia = createPinia();
 app.use(router);
